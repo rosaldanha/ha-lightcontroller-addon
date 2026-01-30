3c1f44f1db894735b6e2b07f5f169edb90d6bddd
Ricardo Saldanha <rosaldanha@gmail.com>
2026-01-30 16:11:42 -0300

    start to implement esphomeconfig class

--- sorj-manager/nuxt.config.ts
+++ sorj-manager/nuxt.config.ts
@@ -1,23 +1,5 @@
 import { defineNuxtConfig } from "nuxt/config";
-import fs from "node:fs";
 
-// Função auxiliar para ler o options.json do Home Assistant
-const getHassOptions = () => {
-  try {
-    // O caminho padrão no Hassio é /data/options.json
-    if (fs.existsSync("/data/options.json")) {
-      const rawData = fs.readFileSync("/data/options.json", "utf-8");
-      console.log(rawData);
-      console.log(JSON.parse(rawData));
-      return JSON.parse(rawData);
-    }
-  } catch (e) {
-    console.warn("Não foi possível ler /data/options.json, usando padrões.");
-  }
-  return {};
-};
-const hassOptions = getHassOptions();
-console.log("loaded hassOptions");
 // sorj-manager/nuxt.config.ts
 export default defineNuxtConfig({
   // 1. SPA Mode: Renderização apenas no navegador
@@ -36,10 +18,7 @@ export default defineNuxtConfig({
   },
   runtimeConfig: {
     // Chaves aqui são PRIVADAS (disponíveis apenas em server/api)
-    esphomeConfigFolder:
-      process.env.ESPHOME_CONFIG_DIR ||
-      hassOptions.esphomeConfigFolder ||
-      "error",
+    esphomeConfigFolder: process.env.ESPHOMECONFIGDIR || "error",
 
     // Se precisar no frontend (vue), coloque dentro de public:
     public: {
