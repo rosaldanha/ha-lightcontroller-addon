7228410dc2f12f3a6225157c93925457d263c21b
Ricardo Saldanha <rosaldanha@gmail.com>
2026-02-02 10:34:42 -0300

    monitormodal

--- upd.sh
+++ upd.sh
@@ -1,5 +1,11 @@
 git add . ; git commit -m $1 ; git push
 xh POST https://hass.sal.net.br/api/hassio/store/refresh \
   "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJjYjBiMmZhNjcyMmQ0YjdiOWE1ZjM4NjgwOTc3YmRkMiIsImlhdCI6MTc3MDAyOTU4NCwiZXhwIjoyMDg1Mzg5NTg0fQ.LqkZG85reW-uDICWw9XKaOlTzD1oUvXozZS-tulY5mA"
-xh POST https://hass.sal.net.br/api/hassio/addons/<SLUG_DO_ADDON>/update \
-    "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJjYjBiMmZhNjcyMmQ0YjdiOWE1ZjM4NjgwOTc3YmRkMiIsImlhdCI6MTc3MDAyOTU4NCwiZXhwIjoyMDg1Mzg5NTg0fQ.LqkZG85reW-uDICWw9XKaOlTzD1oUvXozZS-tulY5mA"
+
+# xh POST https://hass.sal.net.br/api/services/hassio/addon_update \
+#   Authorization:"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJjYjBiMmZhNjcyMmQ0YjdiOWE1ZjM4NjgwOTc3YmRkMiIsImlhdCI6MTc3MDAyOTU4NCwiZXhwIjoyMDg1Mzg5NTg0fQ.LqkZG85reW-uDICWw9XKaOlTzD1oUvXozZS-tulY5mA" \
+#   addon=db25358c_sorj_net_manager
+
+
+# xh POST https://hass.sal.net.br/api/hassio/addons/db25358c_sorj_net_manager/update \
+#     "Authorization: Bearer "
