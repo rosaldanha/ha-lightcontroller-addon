57a932553810318c41f3872b004f6f1e1061fbc1
Ricardo Saldanha <rosaldanha@gmail.com>
2026-02-02 18:03:44 -0300

    monitormodal

--- sorj-manager/components/MonitorModal.vue
+++ sorj-manager/components/MonitorModal.vue
@@ -68,7 +68,7 @@ const connectToHA = async () => {
     // 4. Handle WebSocket messages
     socket.value.onmessage = (event) => {
       const msg = JSON.parse(event.data);
-      eventLog.value.push(event);
+      eventLog.value.push(JSON.stringify(event));
       if (msg.type === "auth_required") {
         socket.value?.send(
           JSON.stringify({
