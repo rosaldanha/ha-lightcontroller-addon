6f7d98f40d9ad7257164455b421fd1c2454dda80
Ricardo Saldanha <rosaldanha@gmail.com>
2026-02-02 10:27:34 -0300

    implementing monitorModal

---
+++ upd.sh
@@ -0,0 +1,5 @@
+git add . ; git commit -m $1 ; git push
+xh POST https://hass.sal.net.br/api/hassio/store/refresh \
+  "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJjYjBiMmZhNjcyMmQ0YjdiOWE1ZjM4NjgwOTc3YmRkMiIsImlhdCI6MTc3MDAyOTU4NCwiZXhwIjoyMDg1Mzg5NTg0fQ.LqkZG85reW-uDICWw9XKaOlTzD1oUvXozZS-tulY5mA"
+xh POST https://hass.sal.net.br/api/hassio/addons/<SLUG_DO_ADDON>/update \
+    "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJjYjBiMmZhNjcyMmQ0YjdiOWE1ZjM4NjgwOTc3YmRkMiIsImlhdCI6MTc3MDAyOTU4NCwiZXhwIjoyMDg1Mzg5NTg0fQ.LqkZG85reW-uDICWw9XKaOlTzD1oUvXozZS-tulY5mA"
