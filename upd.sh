04ff984d1e960af3cc372b4b8507d2b703744d8d
Ricardo Saldanha <rosaldanha@gmail.com>
2026-02-02 11:47:43 -0300

    monitormodal

--- upd.sh
+++ upd.sh
@@ -2,7 +2,7 @@ git add . ; git commit -m $1 ; git push
 # xh POST https://hass.sal.net.br/api/hassio/store/refresh \
 #   Authorization="Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJjYjBiMmZhNjcyMmQ0YjdiOWE1ZjM4NjgwOTc3YmRkMiIsImlhdCI6MTc3MDAyOTU4NCwiZXhwIjoyMDg1Mzg5NTg0fQ.LqkZG85reW-uDICWw9XKaOlTzD1oUvXozZS-tulY5mA"
 
-xh POST https://hass.sal.net.br/store/reload \
+xh POST https://hass.sal.net.br/api/store/reload \
   Authorization:"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJjYjBiMmZhNjcyMmQ0YjdiOWE1ZjM4NjgwOTc3YmRkMiIsImlhdCI6MTc3MDAyOTU4NCwiZXhwIjoyMDg1Mzg5NTg0fQ.LqkZG85reW-uDICWw9XKaOlTzD1oUvXozZS-tulY5mA" \
 #   addon=db25358c_sorj_net_manager
 
