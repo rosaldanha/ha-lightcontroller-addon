490ecd8f802935f53eef7835553e73887a483768
Ricardo Saldanha <rosaldanha@gmail.com>
2026-02-04 10:14:21 -0300

    fix EsphomeConfig dump

--- sorj-manager/server/api/devices.post.ts
+++ sorj-manager/server/api/devices.post.ts
@@ -20,6 +20,7 @@ export default defineEventHandler(async (event) => {
     }
 
     // Use the EsphomeConfig type for the request body
+    console.log("**************** EVENT DIRECT PRINT **********************");
     console.log(event);
     const body = await readBody<EsphomeConfig>(event);
 
