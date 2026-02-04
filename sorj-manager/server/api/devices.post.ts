26ee40e486f0efa1e5498a08df50b5a44df4cf2d
Ricardo Saldanha <rosaldanha@gmail.com>
2026-02-04 09:35:30 -0300

    fix EsphomeConfig dump

--- sorj-manager/server/api/devices.post.ts
+++ sorj-manager/server/api/devices.post.ts
@@ -33,23 +33,23 @@ export default defineEventHandler(async (event) => {
     const dataToDump = body;
 
     // Manually reconstruct the 'packages' property with EsphomeInclude instances
-    if (dataToDump.packages) {
-      for (const key in dataToDump.packages) {
-        const pkg = dataToDump.packages[key] as any;
+    // if (dataToDump.packages) {
+    //   for (const key in dataToDump.packages) {
+    //     const pkg = dataToDump.packages[key] as any;
 
-        if (pkg.data && typeof pkg.data === "string") {
-          // Handles scalar includes: base: !include path/to/file.yaml
-          dataToDump.packages[key] = new EsphomeInclude(pkg.data);
-        } else {
-          // Handles mapping includes: po1: !include { file: ..., vars: ... }
-          const includeData = {
-            file: pkg.file,
-            vars: pkg.vars,
-          };
-          dataToDump.packages[key] = new EsphomeInclude(includeData);
-        }
-      }
-    }
+    //     if (pkg.data && typeof pkg.data === "string") {
+    //       // Handles scalar includes: base: !include path/to/file.yaml
+    //       dataToDump.packages[key] = new EsphomeInclude(pkg.data);
+    //     } else {
+    //       // Handles mapping includes: po1: !include { file: ..., vars: ... }
+    //       const includeData = {
+    //         file: pkg.file,
+    //         vars: pkg.vars,
+    //       };
+    //       dataToDump.packages[key] = new EsphomeInclude(includeData);
+    //     }
+    //   }
+    // }
     const yamlString = yaml.dump(dataToDump, {
       schema: ESPSCHEMA,
     });
